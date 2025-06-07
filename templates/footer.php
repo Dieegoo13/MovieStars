bfadd8c2cea565faa2a8aa4d66ee2ea5379d2699
Diego <151255198+Dieegoo13@users.noreply.github.com>
2025-06-07 20:38:40 -0300

    primeira atualização

---
+++ templates/footer.php
@@ -0,0 +1,21 @@
+    <footer id="footer">
+        <div id="social-container">
+            <ul>
+                <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+            </ul>
+        </div>
+
+        <div id="footer-links-container">
+            <ul>
+                <li><a href="#">Adicionar filme</a></li>
+                <li><a href="#">Adicionar crítica</a></li>
+                <li><a href="#">Entrar / Registrar</a></li>
+            </ul>
+        </div>
+
+        <p> &copy; 2025</p>
+    </footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,92 @@
+<?php 
+    require_once("globals.php");
+    require_once("db.php");
+    require_once("models/Message.php");
+    require_once("dao/UserDAO.php");
+
+    $message = new Message($BASE_URL);
+
+    $flashMessage = $message -> getMessage();
+
+    if(!empty($flashMessage["msg"])){
+        $message -> clearMessage();
+    }
+
+    $userDao = new UserDAO($conn, $BASE_URL);
+
+    $userData = $userDao->verifyToken(false);
+    
+;?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moviestar</title>
+    <link rel="shortcut icon" href="<?= $BASE_URL ?>img/moviestar.ico">
+
+    <!-- font awnseome -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- link bootstrap  -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css" integrity="sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
+    <!-- link css -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+</head>
+<body>
+    <header>
+        <nav id="main-navbar" class="navbar navbar-expand-lg">
+            <a href="<?= $BASE_URL ?>" class="navbar-brand">
+                <img src="<?= $BASE_URL ?>img/logo.svg" alt="moviestar" id="logo">
+
+                <span id="moviestar-title">MovieStar</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+
+            <form action="" method="get" id="search-form" class="d-flex align-items-center gap-2">
+                <input type="text" name="q" id="search" class="form-control" placeholder="Buscar filmes" aria-label="Search">
+                <button class="btn btn-outline-light" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>
+
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav">
+                    <?php if($userData):?>
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>newmovie.php" class="nav-link"><i class="far fa-plus-square"></i>Incluir Filme</a>
+                        </li>  
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>dashboard.php" class="nav-link">Meus Filmes</a>
+                        </li>  
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>editprofile.php" class="nav-link bold">
+                                <?= $userData->name?>
+                            </a>
+                        </li>  
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>logout.php" class="nav-link">Sair</a>
+                        </li>  
+                    <?php else: ?>  
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+                        </li>  
+                    <?php endif;?>    
+                </ul>
+            </div>
+
+        </nav>
+    </header>
+    <?php if (!empty($flashMessage["msg"])): ?>
+        <div class="msg-container">
+        <p class="msg <?= $flashMessage["type"] ?>"><?= $flashMessage["msg"] ?></p>
+        </div>  
+    <?php endif; ?>      
